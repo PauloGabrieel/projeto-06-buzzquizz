702b3badadfd6bcf438b2efaa9a0df9652b529ff
Vinicius Pacheco <vinicioss9955@gmail.com>
2022-04-20 11:36:42 -0300

    Botão concluído

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,1 +1,18 @@
+function criarQuizz() {
+  document.querySelector('.tela1').classList.add('escondido')
+  document.querySelector('.tela3').classList.remove('escondido')
+}
 
+function removerQuizz(elemento) {
+  elemento.remove()
+  verificarSeusQuizzes()
+}
+function verificarSeusQuizzes() {
+  const tamanho = document
+    .querySelector('.containerQuizz')
+    .querySelectorAll('img').length
+  if (tamanho === 0) {
+    document.querySelector('.seusQuizz').classList.toggle('escondido')
+    document.querySelector('.semQuizz').classList.toggle('escondido')
+  }
+}
